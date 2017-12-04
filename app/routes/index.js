09892f7c586d7231a04c0ae3d1f3c78028dbc2c6
scriptchao <1154806236@qq.com>
2017-12-04 19:09:10 +0800

    'blog1.0.0版本优化完毕等待发布!'

--- app/routes/index.js
+++ app/routes/index.js
@@ -32,7 +32,7 @@ class App extends React.Component {
 
         return (
             <div className="JAVASCRIPT">
-                {/*<Canvas />*/}
+                <Canvas />
                 <BackTop
                     visibleHeight={500}
                 />
