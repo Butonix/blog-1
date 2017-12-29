358ada3aa458c558d2b59ef5052c9cc89dbe391b
scriptchao <1154806236@qq.com>
2017-12-29 16:34:51 +0800

    'add'

--- postcss.config.js
+++ postcss.config.js
@@ -12,6 +12,5 @@ module.exports = {
             // browsers: ['last 2 versions']
             browsers: ['last 10 Chrome versions', 'last 5 Firefox versions', 'Safari >= 6', 'ie > 8']
         }),
-        require('postcss-pxtorem')(options)
     ]
 };
