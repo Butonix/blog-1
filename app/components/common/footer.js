2ea184e637f0612b706ff00a6ac542d983804d21
scriptchao <1154806236@qq.com>
2017-12-29 18:06:40 +0800

    '底部新增备案信息'

--- app/components/common/footer.js
+++ app/components/common/footer.js
@@ -5,6 +5,7 @@
 import React from 'react'
 import {observable} from 'mobx'
 import {observer} from 'mobx-react'
+import './footer.sass'
 
 
 @observer
@@ -13,6 +14,10 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer className="com-footer">
+                <p className="copyright">© 2017-2018 scriptchao.com 版权所有 ICP证 : 浙ICP备17059730号-1</p>
+                <p><i>{null}</i><a href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=5176.8142029.631162.98.22449968rWQgOr&recordcode=33010602008935"
+                                   target="_blank">浙公网安备
+                    33010602008935号</a></p>
             </footer>
         )
     }
