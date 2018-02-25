c68b59bf27fc36582100195f34f1526b20147d68
scriptchao <1154806236@qq.com>
2018-02-25 21:56:17 +0800

    'fix-footer'

--- app/components/common/footer.js
+++ app/components/common/footer.js
@@ -3,8 +3,9 @@
  */
 
 import React from 'react';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
+import {enquireScreen} from 'enquire-js'
 import './footer.sass';
 import beian from '../../img/beian.png'
 
@@ -12,13 +13,27 @@ import beian from '../../img/beian.png'
 @observer
 export default class Footer extends React.Component {
 
+    @observable isMobile;
+
+
+    componentDidMount() {
+        enquireScreen((mobile) => {
+            this.isMobile = mobile
+        });
+    }
+
+
     render() {
         return (
             <footer className="footer">
-                <p className="copyright">© 2017-2018 scriptchao.com 版权所有 ICP证 : 浙ICP备17059730号-1</p>
+                {
+                    !this.isMobile ?
+                        <p className="copyright">© 2017-2018 scriptchao.com 版权所有 ICP证 : 浙ICP备17059730号-1</p> : null
+                }
                 <p>
                     <img src={beian}/>
-                    <a href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=5176.8142029.631162.98.22449968rWQgOr&recordcode=33010602008935"
+                    <a
+                        href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=5176.8142029.631162.98.22449968rWQgOr&recordcode=33010602008935"
                         rel="noopener noreferrer"
                         target="_blank">浙公网安备 33010602008935号</a>
                 </p>
